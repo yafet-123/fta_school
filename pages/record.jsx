4bdad97cc8bb5637fba2007f811811e839ad5842
yafet-123 <yafetaddisu123@gmail.com>
2023-11-26 07:00:03 -1200

    edited the data folder

--- pages/record.jsx
+++ pages/record.jsx
@@ -1,28 +1,43 @@
 import { MainHeader } from "../components/common/MainHeader";
 import React, { useEffect } from 'react';
-import dynamic from 'next/dynamic';
-
-// Import Handsontable dynamically to ensure it's only loaded on the client side
-const DynamicHandsontable = dynamic(() => import('handsontable'), { ssr: false });
+import ExcelJS from 'exceljs';
+import Handsontable from 'handsontable';
 
 async function generateExcel() {
-  const buffer = await workbook.xlsx.writeBuffer();/* Your generateExcel logic here */;
-  return Handsontable.utils.json.parse(buffer.toString());
+  const workbook = new ExcelJS.Workbook();
+  const sheet = workbook.addWorksheet('Sheet 1');
+
+  sheet.addRow(['Name', 'Age']);
+  sheet.addRow(['John Doe', 30]);
+  sheet.addRow(['Jane Smith', 25]);
+
+  return workbook.xlsx.writeBuffer();
 }
 
 export default function Record() {
   useEffect(() => {
-    generateExcel().then(data => {
-      // Create Handsontable instance only on the client side
-      const container = document.getElementById('excel-container');
-      const hot = new DynamicHandsontable(container, {
-        data,
+    const container = document.getElementById('excel-container');
+
+    generateExcel().then(buffer => {
+      // Convert Excel buffer to a Blob
+      const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+
+      // Use FileSaver.js to save Blob as a file
+      const url = window.URL.createObjectURL(blob);
+
+      // Create Handsontable instance
+      const hot = new Handsontable(container, {
+        data: Handsontable.helper.excel.readData(url, 'array', blob),
         rowHeaders: true,
-        colHeaders: ['Name', 'Age'],
+        colHeaders: true,
         contextMenu: true,
         licenseKey: 'non-commercial-and-evaluation', // Add your commercial license key if applicable
       });
+
+      // Release the URL object when done
+      window.URL.revokeObjectURL(url);
     });
+
   }, []); 
 
   return (
