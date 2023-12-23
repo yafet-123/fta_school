a34cbe69245834815693a3e3eba44223a7377fc1
yafet-123 <yafetaddisu123@gmail.com>
2023-12-22 14:32:34 -1200

    edited z pages

--- components/question/QuestionCategory.jsx
+++ components/question/QuestionCategory.jsx
@@ -13,8 +13,8 @@ export default function QuestionCategory({AllquestionCategory}) {
       {AllquestionCategory.map((data,index)=>(
         <button 
           onClick={() => handleSubject(data.question_category_id)} 
-          className={`${ data.question_category_id % 2 == 0 && "bg-[#2862E9]" || data.question_category_id % 2 == 1 && "bg-[#EFD81D]" 
-          || data.question_category_id % 2 == 2 && "bg-[#E95F21]" } 
+          className={`${ data.question_category_id % 3 == 0 && "bg-[#2862E9]" || data.question_category_id % 3 == 1 && "bg-[#EFD81D]" 
+          || data.question_category_id % 3 == 2 && "bg-[#E95F21]" } 
           px-5 py-16 text-white font-bold text-xl lg:text-3xl rounded-lg`}
         >
           {data.questioncategoryName}
