b2d4bcb88bf4829a342a120c542a77ed48e68459
yafet-123 <yafetaddisu123@gmail.com>
2024-02-19 14:31:04 -1200

    edited the students page for the mobile view

--- pages/auth/error.js
+++ pages/auth/error.js
@@ -7,11 +7,11 @@ export default function Error () {
   const router = useRouter();
   return(
     <React.Fragment>
-      <MainHeader title="SignIn Error Page" />
+      <MainHeader title="Authentication Error Page" />
       <div className="w-full h-screen bg-gray-100 dark:bg-slate-700 pt-24 grid justify-center content-center ">
         <div className="flex flex-col justify-center items-center px-5 lg:px-32">
           <p className="text-center text-md lg:text-4xl font-bold">
-            Oops! It seems there was an issue accessing this page.
+              Oops! It seems there was an issue accessing this page.
              Please check if you have the necessary permissions or contact support for assistance.
           </p>
         </div>
