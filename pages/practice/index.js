d58a09a85fcf467b3f3a270045e1240141231289
yafet-123 <yafetaddisu123@gmail.com>
2025-09-21 21:39:56 +0300

    added the images

--- pages/practice/index.js
+++ pages/practice/index.js
@@ -2,13 +2,14 @@ import { MainHeader } from "../../components/common/MainHeader";
 import Grade from '../../components/practice/Grade.jsx';
 import QuizPage from '../../components/practice/QuizPage.jsx';
 import {useRouter} from 'next/router'
+import QuizPage from '../../components/practice/QuizPage.jsx';
 
 export default function Book() {
   return (
     <div className="">
       <MainHeader title="MatricMate : Books" />
       <div className="flex flex-col">
-        <Grade />
+        <QuizPage />
       </div>
     </div>
   );
