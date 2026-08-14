b1edff1adca43be8a5c079f8b8be2a3414a1e008
unknown <yafetaddisu123@gmail.com>
2026-08-14 12:46:34 -0700

    edited

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 import { Footer } from '../common/Footer';
 import { Navbar } from '../common/Navbar';
 
-interface LayoutProps {
-  children: React.ReactNode;
-  session?: any;
-  pageProps?: any;
-}
 
-const Layout: React.FC<LayoutProps> = ({ children, session, pageProps }) => {
+const Layout = ({ children, session, pageProps }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar session={session} pageProps={pageProps} />
